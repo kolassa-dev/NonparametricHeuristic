ffc1c306abdc3385c65f81b4818b3ca538150199
kolassa-dev <kolassa@stat.rutgers.edu>
2020-02-09 14:41:50 -0500

    Modified testreg to account for non-zero means.

--- R/common.R
+++ R/common.R
@@ -382,47 +382,6 @@ fun.testregboot<-function(dists,regparam,sampsize=10, mcsamp=10000, B=9999){
    }
    return(out/mcsamp)
 }
-#' Compare coverage of confidence intervals from various regression techniques.  Intervals are nominal coverage .1.
-#'
-#' @param dists vector of names of random number generators for sample.  Defaults to normal.
-#' @param npergp Number of observations per sample; defaults to 10
-#' @param mcsamp Number of Monte Carlo samples.
-#'
-#' @return A matrix of coverage levels.  Dimensions are distribution and confidence interval technique.
-#'
-#' @return A list with two components.  The first is a two-dimensional array of average interval lengths.  The second is coverage levels.  Dimensions of both are technique and distribution.
-#'
-#' @examples
-#' fun.testreg(mcsamp=100)
-#'
-#' @export
-#' @importFrom quantreg rq
-fun.testreg<-function(dists="rnorm",npergp=10,mcsamp=10000){
-   out<-array(NA,c(mcsamp,2,length(dists)))
-   ciends<-array(NA,c(mcsamp,2,2,length(dists)))
-   dimnames(out)<-list(NULL,c("L1","L2"),dists)
-   dimnames(ciends)<-list(NULL,c("L1","L2"),c("l","u"),dists)
-   for(k in seq(length(dists))){
-      thisdist<-eval(parse(text=dists[k]))
-      for(j in seq(dim(out)[1])){
-#        x<-thisdist(npergp)
-         x<-seq(npergp)
-         y<-thisdist(npergp)
-# Confidence intervals for rq are calculated by rq.fit.br inside summary.rq .
-# Pass this as noted in the summary.rq documentation by alpha= in the ...
-# argument.
-         cc<-summary(rq(y~x))$coefficients[2,2:3]
-         ciends[j,1,,k]<-cc
-         out[j,1,k]<-(cc[1]<0)&(cc[2]>0)
-         ss<-summary(lm(y~x))$coefficients[2,]
-         out[j,2,k]<-ss[4]>.1
-         ciends[j,2,,k]<-ss[1]+c(-1,1)*qt(.95,8)*ss[2]
-      }
-   }
-   ciendav<-apply(ciends,2:4,mean)
-   return(list(cover=apply(out,2:3,mean),
-      cil=ciendav[,2,]-ciendav[,1,]))
-}
 #' Collection of location measures
 #' @param y data vector
 #' @returns mean median, and trimmed mean
