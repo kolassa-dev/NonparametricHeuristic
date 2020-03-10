d9886c71e3dfc82f323e1e7341870820374c27f9
kolassa-dev <kolassa@stat.rutgers.edu>
2020-03-09 21:23:09 -0400

    Add achievable

---
+++ R/achievable.R
@@ -0,0 +1,23 @@
+#' List the achievable p-values for various sample sizes.
+fun.achievable<-function(){
+   u<-array(NA,c(41,4))
+   for(n in seq(dim(u)[1])) {
+      v<-rep(NA,(n+1)/2)
+      for(i in seq(length(v))) v[i]<-binom.test(i-1,n)$p.value
+      u[n,2]<-sum(v<=.05)-1
+      if(u[n,2]>-1) u[n,4]<-max(v[v<=.05])
+      w<-v
+      if(n>2){
+         for(i in seq(length(w))) w[i]<-z.test(c(rep(0,n-i),
+            .5,rep(1,i-1)),mu=.5,sigma.x=.5)$p.value
+#        u[n,3]<-max(w[w<=.05])
+         u[n,3]<-sum(w<=.05)-1
+      }
+      u[n,1]<-n
+   }
+   dimnames(u)<-list(NULL,c("n","Exact Critical Value",
+      "Asymptotic Critical Value","Exact Size"))
+#  v<-cbind(u[6:17,],u[18:29,],u[30:41,])
+   v<-cbind(u[6:23,],u[24:41,])
+   return(v)
+}
--- R/common.R
+++ R/common.R
@@ -511,29 +511,6 @@ fun.invertsigntest<-function(dataset,alpha=0.05,maint=NULL,tau=0.5,returndata=FA
    out<-if(returndata) list(ci=ci,dataset=dataset) else ci
    return(out)
 }
-#' List the achievable p-values for various sample sizes.
-fun.achievable<-function(){
-   u<-array(NA,c(41,4))
-   for(n in seq(dim(u)[1])) {
-      v<-rep(NA,(n+1)/2)
-      for(i in seq(length(v))) v[i]<-binom.test(i-1,n)$p.value
-      u[n,2]<-sum(v<=.05)-1
-      if(u[n,2]>-1) u[n,4]<-max(v[v<=.05])
-      w<-v
-      if(n>2){
-         for(i in seq(length(w))) w[i]<-z.test(c(rep(0,n-i),
-            .5,rep(1,i-1)),mu=.5,sigma.x=.5)$p.value
-#        u[n,3]<-max(w[w<=.05])
-         u[n,3]<-sum(w<=.05)-1
-      }
-      u[n,1]<-n
-   }
-   dimnames(u)<-list(NULL,c("n","Exact Critical Value",
-      "Asymptotic Critical Value","Exact Size"))
-#  v<-cbind(u[6:17,],u[18:29,],u[30:41,])
-   v<-cbind(u[6:23,],u[24:41,])
-   return(v)
-}
 #' Siegel-Tukey rank function.
 #' @param x the data vector
 #' @return a vector with the same length as the input, with Siegel-Tukey Ranks.
