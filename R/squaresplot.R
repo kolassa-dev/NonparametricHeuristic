f703368d8e94b442a228be6f7a82a4d1e692e1d2
kolassa-dev <kolassa@stat.rutgers.edu>
2020-07-17 17:49:48 -0400

    Add squares plot

---
+++ R/squaresplot.R
@@ -0,0 +1,15 @@
+squaresplot<-function(x,y,centerx=FALSE,npts=100){
+   if(centerx) x<-x-mean(x)
+   cc<-summary(lm(y~x))$coefficients
+   lims<-outer(cc[,1],c(1,1))+outer(cc[,2],c(-3,3))
+   plot.default(lims[1,],lims[2,],type="n",
+      xlab=dimnames(lims)[[1]][1], ylab=dimnames(lims)[[1]][2])
+   cont<-array(NA,c(npts,npts))
+   bvl<-vector(mode="list",2)
+   for(kk in 1:2) bvl[[kk]]<-lims[kk,]%*%rbind(
+      1-(0:(npts-1))/(npts-1),(0:(npts-1))/(npts-1))
+   for(ii in 1:npts) for(jj in 1:npts)
+      cont[ii,jj]<-mean((y-bvl[[1]][ii]-bvl[[2]][jj]*x)^2)
+   contour.default(bvl[[1]],bvl[[2]],cont,add=TRUE)
+   points.default(cc[1,1],cc[2,1])
+}
