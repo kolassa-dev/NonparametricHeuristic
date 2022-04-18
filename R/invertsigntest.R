962faa40dd85582a963d23182848bd39323d3155
John Kolassa <kolassa@stat.rutgers.edu>
2022-04-18 15:11:18 -0400

    Update invertsigntest

--- R/invertsigntest.R
+++ R/invertsigntest.R
@@ -1,4 +1,4 @@
-#' Diagram for the inversion of the sign test to get confidence intervals for a quantile, or, in the two-sample case, the Hodges-Lehman estimator.
+#' Diagram for the inversion of rank tests to get confidence intervals for a quantile, or, in the two-sample case, location differences.
 #'
 #' @param dataset Data vector, or list of two vectors representing two samples.
 #' @param alpha 1-confidence value; defaults to 0.05
@@ -6,39 +6,68 @@
 #' @param tau quantile to be estimated.  Defaults to 0.5.
 #' @param returndata Logical flag determining whether the data set is included as part of the response.
 #' @param npts number of points at which to evaluate test prior to inversion.
+#' @param hl logical flag to determine whether to use sign or Mood test (FALSE) or the signed rank or rank sum test (TRUE)
+#' @param rescaley logical flag to determine whether to rescale the vertical scale to [0,1].
+#' @param newplot logical flag to start a new plot
 #' @examples
 #' invertsigntest(rnorm(12))
 #' @export
 #' @importFrom stats qwilcox qbinom
 #' @importFrom graphics axis
-invertsigntest<-function(dataset,alpha=0.05,maint=NULL,tau=0.5,returndata=FALSE,npts=10000){
-   hl<-!is.atomic(dataset)
-   if(hl){
+invertsigntest<-function(dataset,alpha=0.05,maint=NULL,tau=0.5,returndata=FALSE,npts=10000,hl=NULL,rescaley=FALSE,newplot=TRUE){
+   twosamp<-!is.atomic(dataset)
+   if(is.null(hl)) hl<-twosamp
+   if(twosamp){
       mm<-length(dataset[[1]])
       nn<-length(dataset[[2]])
       dataset<-as.vector(outer(dataset[[1]],dataset[[2]],"-"))
 #     browser()
    }else{
       nn<-length(dataset)
+      if(hl){
+         temp<-outer(dataset,dataset,"+")/2
+         dataset<-temp[!upper.tri(temp)]
+      }
    }
    rd<-range(dataset)
    mu<-rd[1]-.2*diff(rd)+ (0:npts)/npts*1.4*diff(rd)
    statval<-apply(outer(dataset,mu,"-")>0,2,sum)
-   if(hl){
-      a<-qwilcox(alpha/2,mm,nn)
-      b<-mm*nn+1-qwilcox(alpha/2,mm,nn)
-      message("a=",a," b=",b)
+   if(rescaley){
+      mx<-max(statval)
+      statval<-statval/mx
    }else{
-      a<-qbinom(alpha/2,length(dataset),tau)
-      b<-length(dataset)+1-qbinom(alpha/2,length(dataset),1-tau)
+      mx<-1
    }
-   maintt<-if(hl) "Median Difference" else paste("Quantile",tau,sep=" ")
-   plot(mu,statval,type="l",
-      sub=paste("Confidence",1-alpha,if(hl) "Hodges-Lehman" else "Median",
-         "Interval uses order statistics",a,"and",b,sep=" "),
-      main=paste("Construction of CI for",maint,maintt,sep=" "))
-   abline(h=a,lty=2)
-   abline(h=b-1,lty=2)
+   if(twosamp){
+      if(hl){
+         a<-qwilcox(alpha/2,mm,nn)
+         b<-mm*nn+1-qwilcox(alpha/2,mm,nn)
+         message("a=",a," b=",b)
+      }else{
+         message("Confidence interval construction from the Mood Median Test not yet implemented.")
+         a<-b<-NA
+      }
+   }else{
+      if(hl){
+         a<-qsignrank(alpha/2,nn)
+         b<-nn*(nn+1)/2-a
+      }else{
+         a<-qbinom(alpha/2,length(dataset),tau)
+         b<-length(dataset)+1-qbinom(alpha/2,length(dataset),1-tau)
+      }
+   }
+   maintt<-if(twosamp) "Median Difference" else paste("Quantile",tau,sep=" ")
+   if(newplot){
+      plot(range(mu),range(statval),type="n",
+         ylab=if(twosamp) "Statistic" else "Statistic",
+         xlab=if(twosamp) "Location Difference" else "Location",
+         sub=paste("Confidence",1-alpha,if(twosamp) "Hodges-Lehman" else "Median",
+            "Interval uses order statistics",a,"and",b,sep=" "),
+         main=paste("Construction of CI for",maint,maintt,sep=" "))
+   }
+   lines(mu,statval)
+   abline(h=a/mx,lty=2)
+   abline(h=(b-1)/mx,lty=2)
    axis(4,at=c(a,b-1),labels=c("a","b-1"))
    ci<-sort(dataset)[c(a,b)]
    for(i in 1:2) abline(v=ci[i],lty=3)
